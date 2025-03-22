4c5181e1e1e293420b53b4faca457573ae32f065
0roc4n <esparragojames@gmail.com>
2025-03-22 12:46:09 +0800

    tailwind css setup

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -8,7 +8,7 @@ export type ThemedViewProps = ViewProps & {
 };
 
 export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+  const backgroundColor = useThemeColor({ light: lightColor || '#FFFFFF', dark: darkColor || '#FFFFFF' }, 'background');
 
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
 }
